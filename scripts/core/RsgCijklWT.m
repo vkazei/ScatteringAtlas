3d4a1c6592fb19b095d32b88ffb3d9813d235895
Vladimir Kazei <vkazei@gmail.com>
2018-12-11 09:43:34 +0300

    put WT cases outside of loops for efficiency

--- scripts/core/RsgCijklWT.m
+++ scripts/core/RsgCijklWT.m
@@ -11,14 +11,14 @@ function Rp = RsgCijklWT(sv,gv,cijkl,WT)
 %
 % WT = 'PP';
 % vertical incidence
-% sv = [0;0;1]; 
+% sv = [0;0;1];
 % scattering along the second (y) axis
-% gv = [0;1;0];% 
+% gv = [0;1;0];%
 % Cij = diag([1 1 1 1 1 1]); % mu perturbation
 % mapping it to the tensor notation
 % % cijkl = MS_cij2cijkl(Cij);
 
-% normalization of scattering coefficients is off kappaI = 1 
+% normalization of scattering coefficients is off kappaI = 1
 kappa3 = 1;%3^1.5; % this is the cube of v_p/v_s ratio
 kappa6 = 1;%3^3;
 
@@ -43,26 +43,67 @@ gvSH = (gvSH + regCoeff*rx90*gv)/(norm(gvSH)+regCoeff);
 gvSV = V_cross(gv,gvSH);
 
 %% evaluating the radiation patterns as scattering functions
-% R = \sv\sp : cv : \gv\gp 
-% where 
-% \sv and \sp - are propagation and polarization vectors for incident 
+% R = \sv\sp : cv : \gv\gp
+% where
+% \sv and \sp - are propagation and polarization vectors for incident
 % wavefield, \gv and \gp - same for scattered
 
-for i = 1:3
-    for j = 1:3
-        for k = 1:3
-            for l = 1:3
-                if strcmp(WT,'PP')
+
+if strcmp(WT,'PP')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
                     Rp = Rp + sv(i)*sv(j)*cijkl(i,j,k,l)*gv(k)*gv(l);
-                elseif strcmp(WT,'SHSH')
-                    Rp = Rp + sv(i)*svSH(j)*cijkl(i,j,k,l)*gv(k)*gvSH(l)*kappa6;                    
-                elseif strcmp(WT,'SVSV')                    
+                end
+            end
+        end
+    end
+elseif strcmp(WT,'SHSH')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
+                    Rp = Rp + sv(i)*svSH(j)*cijkl(i,j,k,l)*gv(k)*gvSH(l)*kappa6;
+                end
+            end
+        end
+    end
+elseif strcmp(WT,'SVSV')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
                     Rp = Rp + sv(i)*svSV(j)*cijkl(i,j,k,l)*gvSV(k)*gv(l)*kappa6;
-                elseif strcmp(WT,'PSH')
+                end
+            end
+        end
+    end
+elseif strcmp(WT,'PSH')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
                     Rp = Rp + sv(i)*sv(j)*cijkl(i,j,k,l)*gvSH(k)*gv(l)*kappa3;
-                elseif strcmp(WT,'PSV')
+                end
+            end
+        end
+    end
+elseif strcmp(WT,'PSV')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
                     Rp = Rp + sv(i)*sv(j)*cijkl(i,j,k,l)*gvSV(k)*gv(l)*kappa3;
-                elseif strcmp(WT,'SVSH')                                     
+                end
+            end
+        end
+    end
+elseif strcmp(WT,'SVSH')
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                for l = 1:3
                     Rp = Rp + sv(i)*svSV(j)*cijkl(i,j,k,l)*gvSH(k)*gv(l)*kappa6;
                 end
             end
@@ -73,11 +114,11 @@ end
 end
 
 function [c] = V_cross(a, b)
-    % might be quicker than regular cross
+% might be quicker than regular cross
 
-    c = [a(2).*b(3)-a(3).*b(2)
-         a(3).*b(1)-a(1).*b(3)
-         a(1).*b(2)-a(2).*b(1)];
-    
+c = [a(2).*b(3)-a(3).*b(2)
+    a(3).*b(1)-a(1).*b(3)
+    a(1).*b(2)-a(2).*b(1)];
+
 return
 end
